08ab922c2f9e8969b81089e82bcfd90bd9b718e5
Ezekwu <ezekwujerry@gmail.com>
2026-05-02 20:37:51 +0100

    chore: made fix to navitems and also removed resend api route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: {
-    resendApiKey: process.env.RESEND_API_KEY,
     public: {
       apolloAppId: process.env.APOLLO_APP_ID,
     },
@@ -14,21 +13,5 @@ export default defineNuxtConfig({
     "@nuxtjs/mdc",
     "@nuxt/image",
     "nuxt-svgo",
-    [
-      "nuxt-mail",
-      {
-        message: {
-          to: "ezekwujerry@gmail.com",
-        },
-        smtp: {
-          host: "smtp.resend.com",
-          port: 465,
-          auth: {
-            user: "resend",
-            pass: process.env.RESEND_API_KEY,
-          },
-        },
-      },
-    ],
   ],
 })
